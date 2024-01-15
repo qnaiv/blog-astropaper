2bae177aab2dd97f5a172a86e4fd535a8c6cce73
qnaiv <43468715+qnaiv@users.noreply.github.com>
2024-01-16 00:52:34 +0900

    サイト公開に失敗している問題を修正

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://qnaiv.github.io/", // replace this with your deployed domain
   author: "qnaiv",
   desc: "Tech Onigiri & Tech Tamagoyaki & Tech Tako-san wiener",
   title: "Tech Lunch Box",
